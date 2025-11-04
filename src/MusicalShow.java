0a474ce222e22f96856daea9f71857f154694e5d
Max Solovov <m_solovov1@mail.ru>
2025-11-04 17:28:48 +0300

    Спринт №4

--- src/MusicalShow.java
+++ src/MusicalShow.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-
 public class MusicalShow extends Show{
     private MusicAuthor musicAuthor;
     private String librettoText;
--- src/Theatre.java
+++ src/Theatre.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-
 public class Theatre {
 
     public static void main(String[] args) {
